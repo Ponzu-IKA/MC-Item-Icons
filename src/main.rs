5b0ecb76948a43174e1c6ee18797f490032363ba
Ponzu-IKA <138089174+Ponzu-IKA@users.noreply.github.com>
2025-09-17 17:53:23 +0900

    refactor: Toml-RSの使用をやめ、Clapへ以降

--- src/main.rs
+++ src/main.rs
@@ -1,38 +1,68 @@
-use std::{env, error::Error};
-use serde_derive::Deserialize;
-use std::path::Path;
+use clap::Parser;
+use image::GenericImageView;
+use rayon::prelude::*;
 use std::fs;
-use serde::de::DeserializeOwned;
-use image::{GenericImageView};
+use std::path::{Path, PathBuf};
+use std::time::Instant;
+use std::{error::Error, str::FromStr};
 
 type Result<T> = std::result::Result<T, Box<dyn Error>>;
 
-#[derive(Deserialize)]
-struct Config {
-    itemsize: u32,
-    pos: Pos,
-}
-
-#[derive(Deserialize)]
+#[derive(Debug, Clone)]
 struct Pos {
     x: u32,
     y: u32,
 }
 
-fn deserial_toml<T>(path: &Path) -> Result<T> where T: DeserializeOwned {
-    let file = fs::read_to_string(path)?;
-    let cfg = toml::from_str::<T>(&file)?;
+impl FromStr for Pos {
+    type Err = String;
+    fn from_str(s: &str) -> std::result::Result<Self, Self::Err> {
+        let parts: Vec<&str> = s.split(',').collect();
+        if parts.len() != 2 {
+            return Err(format!("invalid pos: {}", s));
+        }
+        let x = parts[0]
+            .parse()
+            .map_err(|_| format!("invalid x: {}", parts[0]))?;
+        let y = parts[1]
+            .parse()
+            .map_err(|_| format!("invalid y: {}", parts[1]))?;
 
-    Ok(cfg)
+        Ok(Pos { x, y })
+    }
 }
 
-fn you_need_the_help() {
-    println!("つかいかた: mc-item-icons [options]... [file]...");
-    println!("-d     file指定をディレクトリ指定に変更");
-    println!();
+/// Minecraft Item Icon Cropper
+#[derive(Parser, Debug)]
+#[command(author, version, about)]
+struct Args {
+    /// ファイルパス
+    #[arg(short, long, required_unless_present = "directory")]
+    file: Option<String>,
 
-    println!("mc-item-icons     file.png");
-    println!("mc-item-icons -d  fileDir");
+    /// ディレクトリパス
+    #[arg(short, long, required_unless_present = "file")]
+    directory: Option<String>,
+
+    /// 出力パス(デフォルトはoutput)
+    #[arg(short, long, default_value = "output")]
+    output: String,
+
+    /// 背景透過設定(例: "255,255,0")
+    #[arg(short, long)]
+    color: Option<String>,
+
+    /// アイテムフレームサイズ(実際のアイテムがあるわけではない枠の部分)
+    #[arg(short = 'S', long)]
+    framesize: Option<u32>,
+
+    /// アイテムサイズ
+    #[arg(short, long, required = true)]
+    size: u32,
+
+    /// 実際にアイテムリストが始まる位置(--pos x,y)
+    #[arg(long, required = true)]
+    pos: Pos,
 }
 
 fn get_filename(path: &Path) -> String {
@@ -43,64 +73,101 @@ fn get_filename(path: &Path) -> String {
     }
 }
 
-fn file_processor(cfg: &Config, image_path: &Path) -> Result<()> {
+fn file_processor(image_path: &Path, args: &Args) -> Result<()> {
+    println!(
+        "Start processing...: {}",
+        image_path.to_string_lossy().to_string()
+    );
+    let now = Instant::now();
+
     let image = image::open(&image_path)?;
     let (width, height) = image.dimensions();
     let image_name = get_filename(&image_path);
-    
+
     //create output/$image_name directory
     fs::create_dir_all(&format!("output/{}", image_name))?;
-    for x in 0..9 {
-        for y in 0..5 {
-            let x_pos = cfg.pos.x + x * cfg.itemsize;
-            let y_pos = cfg.pos.y + y * cfg.itemsize;
-            if x_pos + cfg.itemsize < width && y_pos + cfg.itemsize < height {
-                let croped_image = image.crop_imm(x_pos, y_pos, cfg.itemsize, cfg.itemsize);
-                let path = format!("output/{}/{}.png", image_name, x + y * 9);
-                croped_image.save(path)?;
+
+    (0..9)
+        .flat_map(|x| (0..5).map(move |y| (x, y)))
+        .enumerate()
+        .par_bridge()
+        .for_each(|(i, (x, y))| {
+            let x_pos = args.pos.x + x * args.size;
+            let y_pos = args.pos.y + y * args.size;
+            if x_pos + args.size < width && y_pos + args.size < height {
+                let sub = image.view(x_pos, y_pos, args.size, args.size);
+
+                let mut path = PathBuf::from(&args.output);
+                path.push(&image_name);
+                path.push(format!("{}.png", i));
+
+                sub.to_image()
+                    .save(path)
+                    .expect("Error: with saving image.");
             } else {
-                eprintln!("File({}) skipped due to attempt to access outside index pixel.", image_path.to_string_lossy());
+                eprintln!(
+                    "File({}) skipped due to attempt to access outside index pixel.",
+                    image_path.to_string_lossy()
+                );
             }
-        }
-    }
+        });
+    let end = now.elapsed();
+    println!(
+        "Process complete:{}",
+        image_path.to_string_lossy().to_string()
+    );
+    println!("time: {:?}", end);
     Ok(())
 }
 
-fn directory_processor(cfg: &Config, directory_path: &Path) -> Result<()> {
+fn directory_processor(directory_path: &Path, args: &Args) -> Result<()> {
+    println!(
+        "Start directory processing...: {}",
+        directory_path.to_string_lossy().to_string()
+    );
+    let now = Instant::now();
+
     for image in fs::read_dir(directory_path)? {
         let image = image?;
         let path = image.path();
-        let _ = file_processor(cfg, &path);
-    };
-    
+        let _ = file_processor(&path, args);
+    }
+
+    println!(
+        "Direcotry process complete:{}",
+        directory_path.to_string_lossy().to_string()
+    );
+    println!("time: {:?}", now.elapsed());
     Ok(())
 }
 
 fn main() -> Result<()> {
-    fs::create_dir_all("output")?;
+    let args = Args::parse();
 
-    let args: Vec<String> = env::args().collect();
-    let config_path = Path::new("config.toml");
-    let config: Config = deserial_toml::<Config>(&config_path)?;
+    println!("Minecraft Item Icon Cropper");
+    println!("===========================");
 
-    match args.get(1).map(|s| s.as_str()) {
-        Some("-d") | Some("--directory") => {
-            match args.get(2).map(|s| s.as_str()) {
-                Some(dir) => {
-                    directory_processor(&config, &Path::new(dir))?;
-                }
-                None => {
-                    eprintln!("エラー: -dオプションの後に値が指定されていません。");
-                }
-            }
-        },
-        Some("-h") | Some("--help") => you_need_the_help(),
-        Some(default) => file_processor(&config, &Path::new(default))?,
-        None => {
-            eprintln!("エラー: 引数がありません。");
-            you_need_the_help();
+    match (&args.file, &args.directory) {
+        (Some(file), None) => {
+            println!("Running with file processing mode.");
+            let path = Path::new(&file);
+            file_processor(&path, &args)?;
         }
-    };
+        (None, Some(dir)) => {
+            println!("Running with directory processing mode.");
+            let path = Path::new(&dir);
+            directory_processor(&path, &args)?;
+        }
+        (Some(file), Some(dir)) => {
+            eprintln!("両方指定されました！");
+            eprintln!("ディレクトリフラグとファイル指定は共存できません。");
+            eprintln!("file: {}, dir: {}", file, dir);
+        }
+        (None, None) => {
+            eprintln!("エラー: file及び--directoryの指定がアリませんでした。");
+            std::process::exit(1);
+        }
+    }
 
     Ok(())
 }
